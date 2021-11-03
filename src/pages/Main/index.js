6290a24644a0b133609c74af5f953f9279f5fd06
Guilherme Saoncella da Silva <guisaoncella@gmail.com>
2021-11-03 13:51:11 -0300

    Estilo pagina Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { FaGithub, FaPlus } from 'react-icons/fa'
+import { Container, Form, SubmitButton } from './styles';
 
 export default function Main(){
     return(
-        <h1>Main</h1>
+        <Container>
+            <h1>
+                <FaGithub size={25} />
+                Meus Repositórios
+            </h1>
+
+            <Form onSubmit={() => {}}>
+                <input type="text" placeholder="Adicionar Repositórios"/>
+
+                <SubmitButton>
+                    <FaPlus color="#FFF" size={14} />
+                </SubmitButton>
+            </Form>
+        </Container>
     )
 }
---
+++ src/pages/Main/styles.js
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    max-width: 700px;
+    background: #FFF;
+    border-radius: 5px;
+    padding: 30px;
+    margin: 80px auto;
+    box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.4);
+
+    h1{
+        font-size: 20px;
+        display: flex;
+        flex-direction: row;
+        
+        svg{
+            margin-right: 10px;
+        }
+    }
+`;
+
+export const Form = styled.form`
+    margin-top: 30px;
+    display: flex;
+    flex-direction: row;
+
+    input{
+        flex: 1;
+        border: 1px solid #DDD;
+        padding: 10px 15px;
+        border-radius: 5px;
+        font-size: 16px;
+    }
+`;
+
+export const SubmitButton = styled.button`
+    background: #0D2636;
+    border: 0;
+    border-radius: 5px;
+    margin-left: 10px;
+    padding: 0 15px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    :hover{
+        background: #253B4A;    
+        transition: background 0.4s;
+    }
+`;
