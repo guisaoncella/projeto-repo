eecd26cc93319f650be0da6b53a08da8cea8fcd4
Guilherme Saoncella da Silva <guisaoncella@gmail.com>
2021-11-03 14:13:00 -0300

    Cria requisição api
    Requisitando da api do github os dados do repositorio digitado

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState, useCallback} from 'react';
 import { FaGithub, FaPlus } from 'react-icons/fa'
 import { Container, Form, SubmitButton } from './styles';
+import api from '../../services/api'
 
 export default function Main(){
+    const [newRepo, setNewRepo] = useState('')
+    const [repositorios, setRepositorios] = useState('')
+
+    const handleSubmit = useCallback((e) => {
+        e.preventDefault()
+        async function submit(){
+            const response = await api.get(`repos/${newRepo}`)
+            const data = {
+                name: response.data.full_name,
+            }
+
+            setRepositorios([...repositorios, data])
+            setNewRepo('')
+        }
+        submit()
+    }, [newRepo, repositorios])
+
     return(
         <Container>
             <h1>
@@ -10,8 +28,8 @@ export default function Main(){
                 Meus Repositórios
             </h1>
 
-            <Form onSubmit={() => {}}>
-                <input type="text" placeholder="Adicionar Repositórios"/>
+            <Form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Adicionar Repositórios" value={newRepo} onChange={e => setNewRepo(e.target.value)} />
 
                 <SubmitButton>
                     <FaPlus color="#FFF" size={14} />
